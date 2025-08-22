dc474173b7389ee3ad7d9066d98b1d58e9c9dbf8
komal panda <komalpanda1510@gmail.com>
2025-08-22 20:51:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from './gemini.js';
 
 const app = express();
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://virtualassistant-frontend-q1ug.onrender.com",
     credentials:true
 }))
 
